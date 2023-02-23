81d7fdc9683e1ea17fd6dc195515436e6c64528d
Dustin Hunsinger <dhunsinger88@gmail.com>
2023-02-23 01:44:03 -0500

    created wagerSchema

---
+++ src/models/contractSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const contractSchema = new mongoose.Schema({
+    creator_id: {type: String, require: true},
+    acceptor_id: {type: String},
+    arbiter_id: {type: String, require: true},
+    terms: {type: String},
+    creator_risk: {type: Number, default: 0},
+    creator_amount_to_accept: {type: Number},
+    expiration: {type: Date},
+    status: {type: String, enum: ['Open', 'Active', 'Pending', 'Closed']}
+});
+
+const model = mongoose.model('Contract', contractSchema);
+
+module.exports = model;
