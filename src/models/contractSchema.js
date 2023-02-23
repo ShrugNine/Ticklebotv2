20fb444b4698f94df66df2c976d3d6c7f7af7f47
Dustin Hunsinger <dhunsinger88@gmail.com>
2023-02-23 02:10:44 -0500

    fixed typo in wager.js

--- src/models/contractSchema.js
+++ src/models/contractSchema.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const contractSchema = new mongoose.Schema({
     creator_id: {type: String, require: true},
     acceptor_id: {type: String},
-    arbiter_id: {type: String, require: true},
     terms: {type: String},
     creator_risk: {type: Number, default: 0},
     creator_amount_to_accept: {type: Number},
