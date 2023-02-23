70c4b3ebcbb5fb2d047f7fc88699a014f5e07f6c
Dustin Hunsinger <dhunsinger88@gmail.com>
2023-02-23 01:24:16 -0500

    began wager schema and added new member creation/message in payout

--- src/functions/helpers/helpPayout.js
+++ src/functions/helpers/helpPayout.js
@@ -19,9 +19,21 @@ module.exports = async (message) => {
   let userAccount;
 
   try {
-    userAccount = await TicklecoinSchema.findOneAndUpdate(filter, update, {
-      upsert: true,
-    });
+    userAccount = await TicklecoinSchema.findOneAndUpdate(filter, update);
+
+    if (!userAccount) {
+      console.log(`Creating new user account for ${message.author.dispaly_name}...`);
+      const newAccount = await TicklecoinSchema.create({
+        user_id: message.user.id,
+        dispaly_name: message.author.dispaly_name,
+        server_id: message.guild.id,
+        current_balance: 1000,
+      });
+      newAccount.save();
+      message.author.send(
+        "Welcome to the Cuddle Bunker! In this server, we use a fake-currency called TickleCoin™. This currency means nothing and can be used to place bets or buy items in the TickleShop to affect other members. You have been granted 1000 TickleCoin™ to get started. You earn a coin for every text-post, and +10 coins for every post with an attached meme. Please feel free to reach out to the moderators with any questions."
+      );
+    }
   } catch (err) {
     console.error(err);
   }
---
+++ src/models/wagerSchema.js
@@ -0,0 +1,12 @@
+// const mongoose = require('mongoose');
+
+// const wagerSchema = new mongoose.Schema({
+//     user_id: { type: String, require: true, unique: true },
+//     server_id: { type: String, require: true },
+//     display_name: String,
+//     current_balance: { type: Number, default: 1000 }
+// });
+
+// const model = mongoose.model('TickleCoinAccount', ticklecoinSchema);
+
+// module.exports = model;
