81eb11f316cdfc888908d12ef2234a9919c163bd
Dustin Hunsinger <dhunsinger88@gmail.com>
2023-02-23 02:28:51 -0500

    fixed wager front end. added bot exception to on message create

--- src/commands/contracts/wager.js
+++ src/commands/contracts/wager.js
@@ -5,46 +5,55 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("wager")
     .setDescription("Create a Wager contract using TickleCoin™")
-    .addStringOption(option => {
-        option.setName = "terms"
-        option.setDescription = "Define in clear terms which conditions would make you the winner of this wager",
-        option.setRequired = true
-    })
-    .addIntegerOption(option => { 
-        option.setName = "creator_risk",
-        option.setDescription = "What you will pay if you lose.",
-        option.setRequired = true})
-    .addIntegerOption(option => {
-        option.setName = "acceptor_risk",
-        option.setDescription = "What the challenger must risk to accept the wager."
-        option.setRequired = true
-    }),
+    .addStringOption((option) =>
+      option
+        .setName("terms")
+        .setDescription(
+          "Define in clear terms which conditions would make you the winner of this wager"
+        )
+        .setRequired(true)
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName("creator_risk")
+        .setDescription("What you will pay if you lose.")
+        .setRequired(true))
+    .addIntegerOption((option) =>
+        option
+        .setName("acceptor_risk")
+        .setDescription(
+            "What the challenger must risk to accept the wager."
+        )
+        .setRequired(true)
+    ),
   async execute(interaction, client) {
     const userId = interaction.user.id;
 
     const account = await TickleCoinAccount.findOne({
-        user_id: userId
-    })
+      user_id: userId,
+    });
 
-    if(!account){
-        await interaction.reply({
-            content: 'There was an issue locating your account. Please try again later.',
-            ephemeral: true
-        });
-        return;
+    if (!account) {
+      await interaction.reply({
+        content:
+          "There was an issue locating your account. Please try again later.",
+        ephemeral: true,
+      });
+      return;
     }
 
-    const wager_amount = interaction.options.getInteger('creator_risk')
+    const wager_amount = interaction.options.getInteger("creator_risk");
 
-    if (account.current_balance < wager_amount){
-        await interaction.reply({
-            content: 'You do not have the funds to make this wager.',
-            ephemeral: true
-        })
-        return;
+    if (account.current_balance < wager_amount) {
+      await interaction.reply({
+        content: "You do not have the funds to make this wager.",
+        ephemeral: true,
+      });
+      return;
     }
 
-    await interaction.reply(`This command does not quite work yet. TO DO: Code to commmit wager to DB. Handler to create visual wager emebed in channel.`);
-
+    await interaction.reply(
+      `This command does not quite work yet. TO DO: Code to commmit wager to DB. Handler to create visual wager emebed in channel.`
+    );
   },
 };
--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -3,6 +3,7 @@ const payout = require('../../functions/helpers/helpPayout.js');
 module.exports = {
   name: "messageCreate",
   async execute(message) {
+    if(!message.author.bot)
     await payout(message); //rewards posts with ticklecoin
   },
 };
