5ed1ad93a3f708b8fd1bd488c1938ff988dd0ce2
Dustin Hunsinger <dhunsinger88@gmail.com>
2023-02-23 20:45:30 -0500

    created leaderboard command

---
+++ src/commands/economy/leaderboard.js
@@ -0,0 +1,16 @@
+const leaderboard = require("./builders/buildLeaderboard.js");
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("leaderboard")
+    .setDescription("Force the leaderboard to update"),
+  async execute(interaction, client) {
+    await leaderboard();
+
+    await interaction.reply({
+      content: `Leaderboard updated!`,
+      ephemeral: true,
+    });
+  },
+};
